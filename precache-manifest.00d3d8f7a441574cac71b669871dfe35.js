dc55ad94a50bc94364335770bcb11916c2927604
Bianca <biancacmatira@gmail.com>
2020-05-15 06:04:33 -0700

    Updates

---
+++ precache-manifest.00d3d8f7a441574cac71b669871dfe35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8aa15c8bb9ec87f0421a026a9cc019",
+    "url": "/react-movie-info-app/index.html"
+  },
+  {
+    "revision": "42b2965adfcd27e0adfa",
+    "url": "/react-movie-info-app/static/css/main.f61ff780.chunk.css"
+  },
+  {
+    "revision": "3d88585ac1691a963828",
+    "url": "/react-movie-info-app/static/js/2.1a0ae4aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie-info-app/static/js/2.1a0ae4aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42b2965adfcd27e0adfa",
+    "url": "/react-movie-info-app/static/js/main.60809bfc.chunk.js"
+  },
+  {
+    "revision": "1c5d9b1be434874c472c",
+    "url": "/react-movie-info-app/static/js/runtime-main.6fc9b8aa.js"
+  }
+]);
