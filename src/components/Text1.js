33791b14f2be9e093452d9a6213539266ee5e508
Bianca <biancacmatira@gmail.com>
2020-05-14 23:43:58 -0700

    initial commit

---
+++ src/components/Text1.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Text1 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Text</h1>
+        <p>text</p>
+      </div>
+    );
+  }
+}
+
+export default Text1;
